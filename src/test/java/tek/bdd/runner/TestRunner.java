ccd414349f6181a2e851ca7b2a7155f02492456d
fatema mirzaee <fatema.na@tekschool.us>
2024-04-08 14:22:27 -0500

    json report

--- src/test/java/tek/bdd/runner/TestRunner.java
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "pretty",
                 "html:target/html_report/index.html",
-                "json:target/cucumber-report/report.json"
+                "json:target/cucumber-report/cucumber-html-reports/report.json"
         }
 
 )
